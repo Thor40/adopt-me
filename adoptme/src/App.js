bf7e113fd60798ed4b483aba6146f499c412de87
Thor40 <nicholas.kosik@gmail.com>
2020-10-03 11:43:20 -0500

    created custom hook

--- adoptme/src/App.js
+++ adoptme/src/App.js
@@ -1,14 +1,12 @@
 import React from "react";
 import { render } from "react-dom";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1 id="something-important">Adopt Me!</h1>
-      <Pet name="Rosie" animal="Dog" breed="Mixed" />
-      <Pet name="Murphy" animal="Dog" breed="Trash-Dog" />
-      <Pet name="Scotia" animal="Dog" breed="Mountian Dog" />
+      <SearchParams />
     </div>
   );
 };
